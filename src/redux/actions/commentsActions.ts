85c6ebf1306226c42ee27a99ce08c966f4222370
VeronikaLushchik <loaaary212@gmail.com>
2021-10-13 17:55:10 +0300

    fixes

--- src/redux/actions/commentsActions.ts
+++ src/redux/actions/commentsActions.ts
@@ -6,9 +6,14 @@ import { postApi } from '../../api';
 
 export const setComments = (comments: Comment[]) => ({ type: SET_COMMENTS, comments });
 
-export const loadComments = (postId:number) => (dispatch: Dispatch) => {
-  postApi.getPostComments(postId)
-    .then((response: AxiosResponse<{}>) => {
-      dispatch(setComments(response.data as Comment[]));
-    });
+export const loadComments = (postId:number) => async (dispatch: Dispatch) => {
+  try {
+    await postApi.getPostComments(postId)
+      .then((response: AxiosResponse<{}>) => {
+        dispatch(setComments(response.data as Comment[]));
+      });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('-------e', e);
+  }
 };
